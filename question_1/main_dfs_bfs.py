17cab52860ade88f56259ee50b3226bd0b6db001
Jefferson Júnior <38020550+Nidoguedur@users.noreply.github.com>
2019-05-05 21:34:34 -0300

    Time method

--- question_1/main_dfs_bfs.py
+++ question_1/main_dfs_bfs.py
@@ -1,6 +1,20 @@
 # coding: utf-8
 from graph.classes import Graph
 from graph.graph_utils import fill_graph, tree_graph
+import time
+
+
+def elapsed_since(start):
+    #return time.strftime("%H:%M:%S", time.gmtime(time.time() - start))
+    elapsed = time.time() - start
+    if elapsed < 1:
+        return str(round(elapsed*1000, 2)) + "ms"
+    if elapsed < 60:
+        return str(round(elapsed, 2)) + "s"
+    if elapsed < 3600:
+        return str(round(elapsed/60, 2)) + "min"
+    else:
+        return str(round(elapsed / 3600, 2)) + "hrs"
 
 
 if __name__ == '__main__':
@@ -9,11 +23,14 @@ if __name__ == '__main__':
 
     vertex_key = input("Informe o vértice inicial para realização das buscas: ")
     try:
+        startTime = time.time()
         g = graph.pop()
-        queue = g.dfs(graph.vertex_list[vertex_key])
-        tree_graph(graph, "dfs", "../question_1/info_graph.txt", queue)
+        queue = g.dfs(g.vertex_list[vertex_key])
+        tree_graph(g, "dfs", "../question_1/info_graph.txt", queue)
+        print("DFS time:" + elapsed_since(startTime))
 
-        g.bfs(graph.vertex_list[vertex_key])
-        tree_graph(graph, "bfs", "../question_1/info_graph.txt")
+        g.bfs(g.vertex_list[vertex_key])
+        tree_graph(g, "bfs", "../question_1/info_graph.txt")
+        print("BFS time:" + elapsed_since(startTime))
     except Exception:
         print("Vértice não encontrado")
